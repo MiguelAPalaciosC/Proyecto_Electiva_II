94ea16afb98c6c6f780e218f6bac44ea90dcef96
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-05-18 18:59:26 -0500

    fix styles mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
   <div class="container m-auto">
     <div class="row m-auto">
       <div class="col-md-6 m-auto">
-        <div class="panel panel-default login-glass">
-          <div class="panel-heading"><strong>Login</strong></div>
-          <div class="panel-body">
+        <div id="login" class="panel login panel-default login-glass">
+          <div class="login panel-heading"><strong>Login</strong></div>
+          <div class="login panel-body">
             <form class="form-horizontal" method="POST" action="{{ route('login') }}">
               {{ csrf_field() }}
               <div class=" row mb-3 form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -22,7 +22,7 @@
                 </div>
               </div>
               <div class="row mb-3 form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" class="col-sm-2 col-form-label"><strong>Password</strong></label>
+                <label for="password" class="col-sm-2 col-form-label"><strong>Contraseña</strong></label>
                 <div class="col-md-10">
                   <input id="password" type="password" class="form-control" name="password" required>
                   @if ($errors->has('password'))
@@ -40,14 +40,9 @@
         </div>
       </div>
       <div class="col-md-6">
-        <img class="img" src="https://image.freepik.com/vector-gratis/estudiantes-felices-libros-excelentes-notas_53562-9975.jpg"/>
+        <img class="img" src="{{ asset('img/login.jpg') }}"/>
       </div>
     </div>
-  </div>
-  <div class="wave" >
-    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-      <path d="M-40.91,36.02 C152.64,-48.84 146.44,91.28 503.10,69.56 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #627fdb;"></path>
-    </svg>
   </div>
 </div>
 @endsection
